515a41b754f7a10602a1d5433671acbb31dcca33
luisarmada <123182885+luisarmada@users.noreply.github.com>
2024-12-15 23:24:46 +0000

    changed to vue

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
