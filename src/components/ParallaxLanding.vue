bf6f6aea5e1a82332ea7c487989928b3b74bf15d
luisarmada <123182885+luisarmada@users.noreply.github.com>
2024-12-16 22:56:47 +0000

    parallax landing animations

--- src/components/ParallaxLanding.vue
+++ src/components/ParallaxLanding.vue
@@ -1,32 +1,39 @@
 <template>
-  <div>
-    <div class="parallax-container">
-      <div
-        class="parallax-layer"
-        v-for="(layer, index) in layers"
-        :key="layer.id"
-        :data-depth="layer.depth"
-        :style="{ backgroundImage: `url(${layer.image})`, animationDelay: `${index * 0.2}s` }"
-      ></div>
-
-      <img
-        id="ctext"
-        src="/images/chinese_callig.png"
-        alt="Chinese Calligraphy"
-        class="reveal-animation"
-      />
-      <img
-        id="abalogo"
-        src="/images/ABACUSLOGO_w.png"
-        alt="ABACUS Logo"
-        class="logo-animation"
-      />
-    </div>
-  </div>
-</template>
-
+    <div>
+      <div class="parallax-container">
+        <!-- Parallax Layers -->
+        <div
+          class="parallax-layer"
+          v-for="(layer, index) in layers"
+          :key="layer.id"
+          :data-depth="layer.depth"
+          :style="{
+            backgroundImage: `url(${layer.image})`,
+            animationDelay: `${index * 0.04}s`
+          }"
+        ></div>
   
-<script>
+        <!-- Chinese Calligraphy Reveal -->
+        <img
+          id="ctext"
+          src="/images/chinese_callig.png"
+          alt="Chinese Calligraphy"
+          class="ctext-animation"
+        />
+  
+        <!-- ABACUS Logo with Full Rotation -->
+        <img
+          id="abalogo"
+          src="/images/ABACUSLOGO_w.png"
+          alt="ABACUS Logo"
+          class="logo-animation"
+        />
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
 export default {
   name: "ParallaxLanding",
   data() {
@@ -108,22 +115,23 @@ export default {
   },
 };
 </script>
-
   
-<style>
-body {
+  <style>
+  body {
     margin: 0;
-    background-color: #D7CAA9;
+    background-color: #d7caa9;
     overflow-x: hidden;
-}
-.parallax-container {
+  }
+  
+  .parallax-container {
     position: relative;
     width: 100vw;
     height: 100vh;
-
     overflow: hidden;
-}
-.parallax-layer {
+  }
+  
+  /* Parallax Layers - Pop Up Animation */
+  .parallax-layer {
     position: absolute;
     top: 0;
     left: -100%;
@@ -131,107 +139,85 @@ body {
     height: 100%;
     background-repeat: repeat-x;
     background-size: auto 100%;
-    transform-origin: center;
-    image-rendering: pixelated;
+    background-position-y: bottom -300px;
+    animation: pop-up 0.8s ease-out forwards;
+  }
+  
+
+@keyframes pop-up {
+  from {
+    background-position-y: bottom -300px;
+  }
+  to {
+    background-position-y: bottom 0px;
+  }
 }
-#abalogo {
+
+  
+  /* ABACUS Logo Animation */
+  #abalogo {
     position: absolute;
     z-index: 3;
     top: 50px;
     left: 50%;
-    transform: translate(-54%, 0%);
+    transform: translate(-50%, 0%) scale(0) rotate(720deg);
+    opacity: 0;
     width: 33%;
     min-width: 400px;
-    user-select: none; 
-    pointer-events: none; 
-    -webkit-user-drag: none;
-}
-#ctext {
+    user-select: none;
+    pointer-events: none;
+    animation: fade-in-scale-rotate 1.5s ease forwards 0s;
+  }
+  
+  @keyframes fade-in-scale-rotate {
+    from {
+      transform: translate(-50%, 0%) scale(0) rotate(-360deg);
+      opacity: 0;
+    }
+    to {
+      transform: translate(-50%, 0%) scale(1) rotate(0deg);
+      opacity: 1;
+    }
+  }
+  
+  /* Chinese Calligraphy Reveal Animation */
+  #ctext {
     position: absolute;
     z-index: 10;
     left: 4%;
-    transform: translate(-7px, 0);
-    top: 0px;
+    top: 0;
     width: 90px;
-    height: auto;
-    user-select: none; 
-    pointer-events: none; 
-    -webkit-user-drag: none;
-}
-@media only screen and (max-width: 1024px) {
+    height: 0; /* Start with height 0 */
+    overflow: hidden;
+    user-select: none;
+    pointer-events: none;
+    animation: reveal-height 1s ease forwards 0.5s;
+  }
+  
+  @keyframes reveal-height {
+    from {
+      height: 0;
+    }
+    to {
+      height: 460px; /* Reveals to the full height of the image */
+    }
+  }
+  
+  @media only screen and (max-width: 1024px) {
     #ctext {
-        display: none;
+      display: none;
     }
     #abalogo {
-        position: absolute;
-        z-index: 3;
-        top: 50px;
-        left: 50%;
-        transform: translate(-52%, 0%);
-        width: 60%;
-        max-width: 400px;
-        user-select: none; 
-        pointer-events: none; 
-        -webkit-user-drag: none;
+      position: absolute;
+      z-index: 3;
+      top: 50px;
+      left: 50%;
+      transform: translate(-52%, 0%);
+      width: 60%;
+      max-width: 400px;
+      user-select: none;
+      pointer-events: none;
     }
-}
-
-#seal {
-    position: absolute;
-    z-index: 10;
-    left: 48px;
-    top: -10px;
-    width: 110px;
-    height: auto;
-    user-select: none; 
-    pointer-events: none; 
-    -webkit-user-drag: none;
-}
-  
-  .myButton {
-    background-color: #32312C;
-    border-radius: 14px;
-    border: 2.5px solid #D7CAA9;
-    display: inline-block;
-    cursor: pointer;
-    color: #D7CAA9;
-    font-family: "Merriweather Sans", sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 16px 31px;
-    text-decoration: none;
-  }
-  .myButton:hover {
-    background-color: #3C3B36;
-  }
-  .myButton:active {
-    position: relative;
-    top: 1px;
-  }
-  .centered_menu {
-    position: absolute;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    top: 72%;
-    width: 100%;
-  }
-  .h_buttons {
-    display: flex;
-    gap: 20px;
-    align-items: center;
-    justify-content: center;
-  }
-  .h_buttons2 {
-    position: absolute;
-    display: flex;
-    gap: 20px;
-    align-items: center;
-    justify-content: center;
-    top: 48%;
-    height: 33%;
-    left: 50%;
-    transform: translate(-50%, 0%);
   }
   </style>
   
